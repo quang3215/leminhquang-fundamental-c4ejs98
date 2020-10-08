583925e0af0d05a9b92ad3efd185b31932b09c7a
lê minh quang <72089951+quang3215@users.noreply.github.com>
2020-10-08 21:52:33 +0700

    Update app5.js

--- app5.js
+++ app5.js
@@ -1,7 +1,7 @@
 let a = prompt("nhập số(,)")
     let aNumber = a.split(",").map(Number)
-    let sum = 0
+    let s = 0
     for (i = 0; i < aNumber.length; i++) {
-        sum += aNumber[i]
+        s += aNumber[i]
     }
-    console.log(`Tổng các số nhập vào là : ${sum}`)
+    console.log(`Tổng các số nhập vào là : ${s}`)
